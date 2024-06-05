8f549fa174c5b31943bf61c423b57fbc64fccac7
KimKyungYun <kky38225221@gmail.com>
2024-06-05 14:25:36 +0900

    feat: 요일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,81 +1,126 @@
-import { useEffect,  useRef,  useState } from 'react'
-import './App.css'
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
 
-function App() {
-  const audioRef = useRef<HTMLAudioElement>(null);
-  const [time, setTime] = useState(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
-  const [method, setMethod] = useState('today');
-
-  const handleTime = () => {
-    const list = time.split(':');
-    const passedTIme =(Number(list[0]) - 9)>=0? ((Number(list[0]) - 9) + ((Number(list[1])) / 60) + ((Number(list[2])) / 3600)) / 9:0;
-    
-    if (method === 'today') return passedTIme>1?100:(Math.floor(passedTIme * 10000000) / 100000).toFixed(5);
-    return (new Date().getDay() - 1 + passedTIme) > 5 ? 5 : (Math.floor((new Date().getDay() - 1 + passedTIme) * 2000000) / 100000).toFixed(5);
-  }
-  
-  
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [])
-  
-  
-  useEffect(() => {
-    if (time === '17:59:52'&&audioRef.current) {
-      
-      audioRef.current.play()
-    }
-  }, [time])
-  
-  return (
-    <>
-      <div>
-       
-      </div>
-      <h1>집에 가고싶다</h1>
-      <h2>{method === 'today' ? '오늘' : '이번주'} 얼마나 남았지..</h2>
-      <div className='method'>
-        <label htmlFor="today">
-          <input id='today' type="radio" name='method' value='today' defaultChecked onClick={() => setMethod('today')} />
-          오늘
-        </label>
-        <label htmlFor="thisweek">
-          <input id='thisweek' type="radio" name='method' value='week' onClick={() => setMethod('week')} />
-          이번주
-        </label>
-      </div>
-      {/* <select className='week' name="week" id="" disabled={method==='today'} onChange={(e)=>setData(prev=>({...prev,week:Number(e.target.value)}))}>
-        <option value={0}>월</option>
-        <option value={1}>화</option>
-        <option value={2}>수</option>
-        <option value={3}>목</option>
-        <option value={4}>금</option>
-      </select>
-
-      <div className='time'>
-        <select name="hour" id="" value={data.hour} onChange={(e) => {
-          if (e.target.value === '9')            setData(prev => ({ ...prev, hour: Number(e.target.value),minute:0 }))
-          setData(prev => ({ ...prev, hour: Number(e.target.value) }))
-        }}>
-          {Array.from({ length: 10 }).map((_, index) =>
-            <option key={index} value={index}>{index+9}시</option>
-          )}
-        </select>
-        <select name="minute" id="" value={data.minute} onChange={(e)=>setData(prev=>({...prev,minute:Number(e.target.value)}))}>
-          {Array.from({ length: 60 }).map((_, index) =>
-            <option key={index} value={index}>{index}분</option>
-          )}
-        </select>
-      </div>      */}
-      <audio src="/timeout.mp3" ref={audioRef} ></audio>
-      {Number(handleTime())>100&&<div style={{ color: '#ff0000', fontSize: '20px', fontWeight: '600' }}>초과근무</div>}
-      <div>{handleTime()}% 완료</div>
-    </>
-  )
+interface WeekType {
+	월: boolean;
+	화: boolean;
+	수: boolean;
+	목: boolean;
+	금: boolean;
 }
 
-export default App
+const DAY_NAMES = ['일', '월', '화', '수', '목', '금', '토'];
+function App() {
+	const WEEKS: Array<string> = ['월', '화', '수', '목', '금'];
+	const [activeDate, setActiveDate] = useState<WeekType>({
+		월: true,
+		화: true,
+		수: true,
+		목: true,
+		금: true,
+  });
+
+  const passedDay=(Object.values(activeDate).slice(0,(new Date().getDay()-1>0?new Date().getDay()-1:0)).filter(value=>value).length);
+  
+  
+	const audioRef = useRef<HTMLAudioElement>(null);
+	const [time, setTime] = useState(
+		`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+	);
+	const [method, setMethod] = useState('today');
+
+	const handleTime = () => {
+		const list = time.split(':');
+    const passedTIme =
+      (activeDate[DAY_NAMES[new Date().getDay()] as keyof WeekType])?
+			(Number(list[0]) - 9 >= 0
+				? (Number(list[0]) -
+						9 +
+						Number(list[1]) / 60 +
+						Number(list[2]) / 3600) /
+				9
+				: 0):0;
+
+    if (method === 'today') {      
+			return passedTIme > 1
+				? 100
+				: (Math.floor(passedTIme * 10000000) / 100000).toFixed(5);
+    }
+    return passedDay + passedTIme > Object.values(activeDate).filter(value => value).length
+			? Object.values(activeDate).filter(value => value).length
+			: (
+					Math.floor((passedDay + passedTIme) * 10000000)/Object.values(activeDate).filter(value => value).length / 100000
+			).toFixed(5);
+	};
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setTime(
+				`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`
+			);
+		}, 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	useEffect(() => {
+		if (time === '17:59:52' && audioRef.current) {
+			audioRef.current.play();
+		}
+	}, [time]);
+
+	return (
+		<>
+			<div></div>
+			<h1>집에 가고싶다</h1>
+			<h2>{method === 'today' ? '오늘' : '이번주'} 얼마나 남았지..</h2>
+			<div className='method'>
+				<label htmlFor='today'>
+					<input
+						id='today'
+						type='radio'
+						name='method'
+						value='today'
+						defaultChecked
+						onClick={() => setMethod('today')}
+					/>
+					오늘
+				</label>
+				<label htmlFor='thisweek'>
+					<input
+						id='thisweek'
+						type='radio'
+						name='method'
+						value='week'
+						onClick={() => setMethod('week')}
+					/>
+					이번주
+				</label>
+			</div>
+
+			<div className='weeks'>
+				{WEEKS.map((value) => (
+					<button
+						className={`weeks-default ${activeDate[value as keyof WeekType] && 'weeks-active'  }`
+							
+						}
+            type='button'
+            onClick={()=>setActiveDate(prev=>({...prev,[value]:!prev[value as keyof WeekType]}))}
+					>
+						{value}
+					</button>
+				))}
+			</div>
+
+			<audio src='/timeout.mp3' ref={audioRef}></audio>
+			{Number(handleTime()) > 100 && (
+				<div style={{ color: '#ff0000', fontSize: '20px', fontWeight: '600' }}>
+					초과근무
+				</div>
+			)}
+			<div>{handleTime()}% 완료</div>
+		</>
+	);
+}
+
+export default App;
